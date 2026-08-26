e9f0edfe9b4fd3d3495993542c7f73877968f89a
radioistj-lab <radioistj@gmail.com>
2026-08-27 00:01:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /*
-  Prompteur vocal — Service Worker V5
+  Prompteur vocal — Service Worker V5.1
   © 2026 C. Declerck
 */
 
-const CACHE_NAME = "prompteur-vocal-v5";
+const CACHE_NAME = "prompteur-vocal-v5-1";
 
 const BASE_URL = new URL("./", self.location.href);
 
